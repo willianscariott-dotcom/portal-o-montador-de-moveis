8c78709d25e1f372557824eb2b13401021e86988
Willian Scariott <contato@grupows.com>
2026-05-01 21:59:29 -0300

    chore: remove Vercel adapter and server-side rendering configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind(), sitemap()],
-  adapter: vercel(),
   base: '/',
   site: 'https://portal.omontadordemoveis.com',
 });

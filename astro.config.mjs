1c82d496df1a12b6bd9f8368b72f14d175d53ae6
Willian Scariott <contato@grupows.com>
2026-05-01 21:54:28 -0300

    feat: add Vercel adapter for deployment compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
+  adapter: vercel(),
   base: '/',
   site: 'https://portal.omontadordemoveis.com',
 });
